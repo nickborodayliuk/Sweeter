fab929d66c9f681378f369690dfec7760a49c642
Nick Borodayliuk <paragamelogict.gmail.com>
2021-03-07 01:44:24 +0200

    add users

--- src/main/java/com/example/sweater/config/MvcConfig.java
+++ src/main/java/com/example/sweater/config/MvcConfig.java
@@ -1,4 +1,13 @@
 package com.example.sweater.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class MvcConfig {
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
 }
--- src/main/java/com/example/sweater/repos/UserRepo.java
+++ src/main/java/com/example/sweater/repos/UserRepo.java
@@ -1,4 +1,8 @@
 package com.example.sweater.repos;
 
-public interface UserRepo {
+import com.example.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
